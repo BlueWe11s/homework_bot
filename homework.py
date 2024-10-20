38d8234e8b440dd1dc32ff995a893edfe61e21bc
BlueWe11s <lshepetr@gmail.com>
2024-10-20 23:51:28 +0300

    first a

--- homework.py
+++ homework.py
@@ -1,11 +1,29 @@
-...
+from dotenv import load_dotenv
+import os
+import requests
+from telebot import TeleBot
+import logging
+import time
+from datetime import datetime
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log', 
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(
+    logging.StreamHandler()
+)
+
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,48 +37,88 @@ HOMEWORK_VERDICTS = {
 }
 
 
+class EmptyDictionaryError(Exception):
+    """Пустой словарь"""
+
+
+class UndocumentedStatusError(Exception):
+    """Недокументированный статус"""
+
+
+class ApiWrongFormat(Exception):
+    """Полученный API неверного формата"""
+
+
 def check_tokens():
-    ...
+    """Проверка есть ли вся нужная информация"""
+    no_tokens = (
+        'Программа принудительно остановлена. '
+        'Отсутствует обязательная переменная окружения:')
+    if PRACTICUM_TOKEN is None:
+        logger.critical(
+            f'{no_tokens} PRACTICUM_TOKEN')
+        exit()
+    elif TELEGRAM_TOKEN is None:
+        logger.critical(
+            f'{no_tokens} TELEGRAM_TOKEN')
+        exit()
+    elif TELEGRAM_CHAT_ID is None:
+        logger.critical(
+            f'{no_tokens} CHAT_ID')
+        exit()
+    else:
+        return True
 
 
 def send_message(bot, message):
-    ...
+    """Отправка сообщения"""
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def get_api_answer(timestamp):
-    ...
+    """Получение ответа от YandexP API"""
+    try:
+        homework_statuses = requests.get(ENDPOINT, headers=HEADERS,
+                                         params={'from_date': 0})
+    except Exception as error:
+        raise error
+
+    return homework_statuses.json()
 
 
 def check_response(response):
-    ...
+    if 'homeworks' not in response or 'current_date' not in response:
+        raise ApiWrongFormat('Полученный API неверного формата')
 
 
 def parse_status(homework):
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    """Анализ изменения"""
+    # status = homework.get('status')
+    # homework_name = homework.get('homework_name')
+    # if status is None:
+    #     code_api = f'Ошибка пустое значение status: {status}'
+    #     logger.error(code_api)
+    #     raise UndocumentedStatusError(code_api)
+    # elif homework_name is None:
+    #     code_api = f'Ошибка пустое значение homework_name: {homework_name}'
+    #     logger.error(code_api)
+    #     raise UndocumentedStatusError(code_api)
+    # verdict = HOMEWORK_VERDICTS[status]
+    verdict = HOMEWORK_VERDICTS[homework['status']]
+    return (f'Изменился статус проверки работы '
+            f'"{homework["homework_name"]}". {verdict}')
+    # return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
-
-    ...
-
-    while True:
-        try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    timestamp = 0
+    check_tokens()
+    response = get_api_answer(timestamp)
+    # check_response(response)
+    message_text = parse_status(response['homeworks'][0])
+    send_message(bot, message_text)
 
 
 if __name__ == '__main__':
