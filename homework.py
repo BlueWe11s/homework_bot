0ecf5c44218ee214fd42ca711d04ff06caa8da41
BlueWe11s <lshepetr@gmail.com>
2024-10-22 09:21:54 +0300

    комит для тех кто забыл

--- homework.py
+++ homework.py
@@ -4,7 +4,8 @@ import requests
 from telebot import TeleBot
 import logging
 import time
-from datetime import datetime
+from http import HTTPStatus
+import json
 
 
 load_dotenv()
@@ -12,7 +13,7 @@ load_dotenv()
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='main.log', 
+    filename='main.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
 )
 logger = logging.getLogger(__name__)
@@ -49,6 +50,18 @@ class ApiWrongFormat(Exception):
     """Полученный API неверного формата"""
 
 
+class IncorrectStatusRequest(Exception):
+    """Статус запроса не 200"""
+
+
+class IncorrectAPIRequest(Exception):
+    """Ошибка при выполнении запроса"""
+
+
+class MessageSendError(Exception):
+    """Ошибка отправки сообщения"""
+
+
 def check_tokens():
     """Проверка есть ли вся нужная информация"""
     no_tokens = (
@@ -72,53 +85,77 @@ def check_tokens():
 
 def send_message(bot, message):
     """Отправка сообщения"""
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except requests.RequestException as error:
+        raise MessageSendError(f'Ошибка отправки сообщения {error}')
+    else:
+        logger.debug('Message send')
 
 
 def get_api_answer(timestamp):
     """Получение ответа от YandexP API"""
     try:
-        homework_statuses = requests.get(ENDPOINT, headers=HEADERS,
-                                         params={'from_date': 0})
-    except Exception as error:
-        raise error
+        response = requests.get(ENDPOINT, headers=HEADERS,
+                                         params={'from_date': timestamp})
+        if response.status_code != HTTPStatus.OK:
+            raise IncorrectStatusRequest('Статус запроса не 200')
+    except requests.RequestException as error:
+        raise IncorrectAPIRequest(f'Ошибка при выполнении запроса: {error}')
+    except json.JSONDecodeError as error:
+        raise ValueError(f'Данные не допустимы {error}')
 
-    return homework_statuses.json()
+    return response.json()
 
 
 def check_response(response):
-    if 'homeworks' not in response or 'current_date' not in response:
-        raise ApiWrongFormat('Полученный API неверного формата')
+    """Проверка API на правильность"""
+    if not isinstance(response, dict):
+        raise TypeError('Неверный тип данных,'
+                        f'{type(response)}')
+    elif 'homeworks' not in response:
+        raise KeyError('В ответе API отсутствует ключ homeworks')
+    elif not isinstance(response['homeworks'], list):
+        raise TypeError('Неверный тип данных по ключу homeworks,'
+                        f'{type(response)}')
+    elif not isinstance(response['current_date'], int):
+        raise TypeError('Неверный тип данных по ключу current_date,'
+                        f'{type(response)}')
+    return response.get('homeworks')
 
 
 def parse_status(homework):
     """Анализ изменения"""
-    # status = homework.get('status')
-    # homework_name = homework.get('homework_name')
-    # if status is None:
-    #     code_api = f'Ошибка пустое значение status: {status}'
-    #     logger.error(code_api)
-    #     raise UndocumentedStatusError(code_api)
-    # elif homework_name is None:
-    #     code_api = f'Ошибка пустое значение homework_name: {homework_name}'
-    #     logger.error(code_api)
-    #     raise UndocumentedStatusError(code_api)
-    # verdict = HOMEWORK_VERDICTS[status]
+    status = homework.get('status')
+    homework_name = homework.get('homework_name')
+    if status is None:
+        code_api = f'Ошибка пустое значение status: {status}'
+        logger.error(code_api)
+        raise UndocumentedStatusError(code_api)
+    elif homework_name is None:
+        code_api = f'Ошибка пустое значение homework_name: {homework_name}'
+        logger.error(code_api)
+        raise UndocumentedStatusError(code_api)
+    elif status not in HOMEWORK_VERDICTS:
+        code_api = f'Ошибка невозможное значение status: {status}'
+        logger.error(code_api)
+        raise UndocumentedStatusError(code_api)
     verdict = HOMEWORK_VERDICTS[homework['status']]
     return (f'Изменился статус проверки работы '
             f'"{homework["homework_name"]}". {verdict}')
-    # return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
     bot = TeleBot(token=TELEGRAM_TOKEN)
-    timestamp = 0
+    timestamp = int(time.time()) - 500
     check_tokens()
-    response = get_api_answer(timestamp)
-    # check_response(response)
-    message_text = parse_status(response['homeworks'][0])
-    send_message(bot, message_text)
+    while True:
+        response = get_api_answer(timestamp)
+        check_response(response)
+        message_text = parse_status(response['homeworks'][0])
+        send_message(bot, message_text)
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
